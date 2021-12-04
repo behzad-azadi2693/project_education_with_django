596e9946c34c726b026225f9fa474614b4172498
joe <48598137+joeliberal@users.noreply.github.com>
2021-12-04 15:44:28 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,10 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #local
-    'education.apps.EducationConfig',
-    'accounts.apps.AccountsConfig',
+    
     'django.contrib.sitemaps',
-    'api.apps.ApiConfig',
     #3rd
     'django_user_agents',
     'rest_framework',
