f3296351ae34dac95d49784f6c845d4cd55787a5
joe <48598137+joeliberal@users.noreply.github.com>
2022-01-06 16:50:28 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import activate_account, password_confirm_down
 from api.views_user import rest_activate_account
+from django.conf.urls.i18n import i18n_patterns
 
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('education.urls')),
     path('accounts/', include('accounts.urls')),
@@ -29,7 +30,7 @@ urlpatterns = [
     path('rest/activate/<uidb64>/<token>/', rest_activate_account, name='rest_activate'),
     path('password/confirm/<uidb64>/<token>/', password_confirm_down, name='password_confirm'),
     path('api/', include('api.urls')),
-]
+)
 
 if settings.DEBUG:
     urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
