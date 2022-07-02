93b013b09b512253dcf43e548cb43d854d7728da
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-07-03 01:23:31 +0430

    Update create_database.sh

--- create_database.sh
+++ create_database.sh
@@ -3,21 +3,23 @@
 set -e
 set -u
 
-
 function create_user_and_database() {
-	local database=$1
-	echo "  Creating user and database '$database'"
+	local database=$(echo $1 | tr ',' ' ' | awk  '{print $1}')
+	local owner=$(echo $1 | tr ',' ' ' | awk  '{print $2}')
+	local passwd=$(echo $1 | tr ',' ' ' | awk  '{print $3}')
+	echo "  Creating user '$owner' and database '$database' "
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $POSTGRES_USER;
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO postgres;
+	    create user $owner with encrypted password '$passwd';
+	    create database $database;
+	    grant all privileges on database $database to $owner;
 EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ':' ' '); do
 		create_user_and_database $db
 	done
 	echo "Multiple databases created"
 fi
+
